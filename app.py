6b20467b9868e8ba1a57657f56cf22144d9b5257
leekj601 <leekj601@naver.com>
2023-07-17 10:30:36 +0900

    logo 바꾼거

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def ocr():
 def index():
     return render_template('index.html')
 
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
 if __name__ == '__main__':
     app.run()
