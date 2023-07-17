abd0f4e55f640f4b4722b581e80f5277b8b3afd8
leekj601 <leekj601@naver.com>
2023-07-17 14:17:53 +0900

    메인 홈페이지(css제외)
    메인 홈페이지 수정본

--- app.py
+++ app.py
@@ -14,9 +14,17 @@ def ocr():
 def index():
     return render_template('index.html')
 
+posts = [
+    {"title": "글 제목 1", "content": "글 내용 1", "image": "/static/images/image1.jpg"},
+    {"title": "글 제목 2", "content": "글 내용 2", "image": "/static/images/image2.jpg"},
+    {"title": "글 제목 3", "content": "글 내용 3", "image": "/static/images/image3.jpg"},
+    {"title": "글 제목 4", "content": "글 내용 4", "image": "/static/images/image4.jpg"},
+    {"title": "글 제목 5", "content": "글 내용 5", "image": "/static/images/image5.jpg"}
+]
+
 @app.route('/main')
 def main():
-    return render_template('main.html')
+    return render_template('main.html',posts=posts)
 
 if __name__ == '__main__':
     app.run()
