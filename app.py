0705fac61ca2a6c1ed2277dc68d8b475f9c42ace
hz6450 <59460811+hz6450@users.noreply.github.com>
2023-07-18 16:29:28 +0900

    index2추가

--- app.py
+++ app.py
@@ -70,6 +70,9 @@ def ocr():
 @app.route('/index')
 def index():
     return render_template('index.html')
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
 
 @app.route('/login')
 def login():
